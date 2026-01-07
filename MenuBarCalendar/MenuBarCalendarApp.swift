a29995075f6a267167dc5dd3e31c9480878ede75
lafcadio5th <jamkaya@gmail.com>
2026-01-07 09:14:03 +0800

    Initial Commit

---
+++ MenuBarCalendar/MenuBarCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarCalendarApp.swift
+//  MenuBarCalendar
+//
+//  Created by Kelvin Tan on 2026/1/7.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
